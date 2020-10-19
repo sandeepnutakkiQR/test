1fe5257e9254b3552d101e1078e6dbb4620595e2
Sandeep Chandra Nutakki <100372@qr.qrgrp.local>
2020-10-19 17:00:13 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please double check the word"
+
+word = input("Enter word: ")
+
+print(translate(word))
+
